b85c7b31826445e09d9a8d79f96602fbc7966ed4
Dzmitry Kasinets <dmitryledger@gmail.com>
2025-02-17 22:49:25 -0500

    Finished

--- delphi/delphiCustomVisitor.java
+++ delphi/delphiCustomVisitor.java
@@ -1,6 +1,10 @@
 import org.antlr.v4.runtime.tree.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class delphiCustomVisitor extends delphiBaseVisitor<Void> {
+    private final Map<String, Integer> fieldValues = new HashMap<>();
+
     @Override
     public Void visitConstructorImplementation(delphiParser.ConstructorImplementationContext ctx) {
         return visitChildren(ctx);
@@ -18,10 +22,14 @@ public class delphiCustomVisitor extends delphiBaseVisitor<Void> {
     @Override
     public Void visitWritelnCall(delphiParser.WritelnCallContext ctx) {
         String value = ctx.expression().getText();
-        try {
-            System.out.println(Integer.parseInt(value));
-        } catch (NumberFormatException e) {
-            System.out.println(value);
+        if (fieldValues.containsKey(value)) {
+            System.out.println(fieldValues.get(value));
+        } else {
+            try {
+                System.out.println(Integer.parseInt(value));
+            } catch (NumberFormatException e) {
+                System.out.println(fieldValues.getOrDefault(value, 0)); // Default to 0 if not found
+            }
         }
         return null;
     }
@@ -43,6 +51,27 @@ public class delphiCustomVisitor extends delphiBaseVisitor<Void> {
 
     @Override
     public Void visitAssignment(delphiParser.AssignmentContext ctx) {
-        return visitChildren(ctx);
+        String varName = ctx.IDENT().getText();
+        String value = ctx.expression().getText();
+        
+        try {
+            fieldValues.put(varName, Integer.parseInt(value));
+        } catch (NumberFormatException e) {
+            // Handle non-integer values if necessary
+        }
+        return null;
+    }
+    
+    @Override
+    public Void visitVariableAssignment(delphiParser.VariableAssignmentContext ctx) {
+        String varName = ctx.IDENT().getText();
+        String value = ctx.expression().getText();
+        
+        try {
+            fieldValues.put(varName, Integer.parseInt(value));
+        } catch (NumberFormatException e) {
+            fieldValues.put(varName, fieldValues.getOrDefault(value, 0));
+        }
+        return null;
     }
 }
